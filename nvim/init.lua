06295ae912e6f19a30f89c2a0e089f3c18c254bc
Grzegorz Brzeczek <gbrzeczek@arraymarketing.com>
2024-12-02 11:19:50 +0100

    feat: add mapping to show registers

--- nvim/init.lua
+++ nvim/init.lua
@@ -108,6 +108,7 @@ map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
 map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
 map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts)
 map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts)
+map('n', '<leader>v', '<cmd>Telescope registers<cr>', opts)
 
 -- Neotree binding
 map('n', '<Leader>n', ':Neotree position=float toggle<CR>', opts)
