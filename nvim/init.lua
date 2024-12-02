d54e5509b5d92a6cb87ba2ebd3e4a6a8287d46da
Grzegorz Brzeczek <gbrzeczek@arraymarketing.com>
2024-12-02 10:50:45 +0100

    fix: eslint on windows

--- nvim/init.lua
+++ nvim/init.lua
@@ -320,7 +320,16 @@ require'lspconfig'.zls.setup {
     on_attach = on_attach
 }
 
-require'lspconfig'.eslint.setup{}
+require"lspconfig".eslint.setup{
+    capabilities = capabilities,
+    on_attach = on_attach,
+    on_new_config = function(config, new_root_dir)
+    config.settings.workspaceFolder = {
+        uri = vim.uri_from_fname(new_root_dir),
+        name = vim.fn.fnamemodify(new_root_dir, ':t')
+    }
+    end,
+}
 
 vim.g.rustaceanvim = {
     server = {
