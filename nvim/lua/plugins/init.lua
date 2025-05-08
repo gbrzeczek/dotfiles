9d9204c394a5025ed77ecb88cd07ee9f9b71759b
Grzegorz Brzeczek <grzegorzbrzeczek1@proton.me>
2025-05-08 08:58:07 +0200

    add tiny-inline-diagnostic

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -143,5 +143,14 @@ return {
           fuzzy = { implementation = "prefer_rust_with_warning" }
         },
         opts_extend = { "sources.default" }
+    },
+    {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        event = "VeryLazy",
+        priority = 1000,
+        config = function()
+            require('tiny-inline-diagnostic').setup()
+            vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+        end
     }
 }
